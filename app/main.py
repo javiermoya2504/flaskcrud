ae78988748cdb24b63b47f7e2bf8ba92c541430d
Javier Moya <168685523+javiermoya2504@users.noreply.github.com>
2026-09-09 11:16:22 -0600

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,11 +1,6 @@
 from app import app
 from contacts import contacts
 
-#cambio de juanito perez
-##323rq4wr34wr43r34r2342343
-"#"#!"#123123"
-#####rergregergreg 1000 lineas
-
 
 app.register_blueprint(contacts)
 
