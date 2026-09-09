5d901efa8047b229fbfd5317c03ebce673ffb08a
Javier Moya <168685523+javiermoya2504@users.noreply.github.com>
2026-09-09 08:47:32 -0600

    34r43r

--- app/main.py
+++ app/main.py
@@ -1,16 +1,9 @@
 from app import app
 from contacts import contacts
 
-#cambio de tu compañero 
-
-#rgrfgewfwewfewfewfew##
-####### jjgljhgjhkgkjgjk
-
-
-#nerkgnrejgn mil lineas 
 
 app.register_blueprint(contacts)
 
 # starting the app
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8080, debug=True)
+    app.run(host="0.0.0.0", port=8081, debug=True)
